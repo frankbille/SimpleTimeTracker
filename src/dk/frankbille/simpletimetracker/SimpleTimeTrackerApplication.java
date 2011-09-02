cd460bd880cfa72a3194b668b3527df9f76d3a3f
Frank Bille <simpletimetracker@frankbille.dk>
2011-09-02 19:30:18 +0200

    Rename the rest application, to indicate that it is a REST application, as well as restructure the package names.

--- src/dk/frankbille/simpletimetracker/SimpleTimeTrackerApplication.java
+++
@@ -1,7 +0,0 @@
-package dk.frankbille.simpletimetracker;
-
-import org.restlet.Application;
-
-public class SimpleTimeTrackerApplication extends Application {
-
-}
---
+++ src/dk/frankbille/simpletimetracker/api/rest/SimpleTimeTrackerRestApplication.java
@@ -0,0 +1,7 @@
+package dk.frankbille.simpletimetracker.api.rest;
+
+import org.restlet.Application;
+
+public class SimpleTimeTrackerRestApplication extends Application {
+
+}
